82b54e4b99d9f8f8d422c75e551ee7ef0c7f1abb
NovichenLestari <80265649+NovichenLestari@users.noreply.github.com>
2021-04-05 20:25:35 +0700

    Add files via upload

---
+++ pertemuan 6/Teori/Transportasi/Bicycle.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author ACER
+ */
+public class Bicycle {
+
+    public void Bicycle(){
+        System.out.println("Ini adalah class Sepeda...");
+    }
+}
---
+++ pertemuan 6/Teori/Transportasi/TransportasiDemo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author ACER
+ */
+import Transportasi.Mobil;
+import Transportasi.Bicycle;
+public class TransportasiDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mobil car = new Mobil();
+        car.Mobil();
+        System.out.println("=========");
+        
+        Bicycle By = new Bicycle();
+        By.Bicycle();
+        System.out.println("=========");
+    }
+    
+}
